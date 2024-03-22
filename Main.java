e4f40523c43e914d4b08f90ed50e05b187bb296a
ProyectosEAFIT <daarismenv@eafit.edu.co>
2024-03-21 20:10:25 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+  public static void Main(String[] args){
+
+    System.out.println("El proyecto se encuentra en https://gallery.appinventor.mit.edu/?galleryid=6476639a-7baa-4cc1-a435-6ed9f062c588");
+    
+  }
+}
