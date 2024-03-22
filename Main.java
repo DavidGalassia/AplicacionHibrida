ef4d89503cd41651dc4d95c0daf1f8d66db40857
Matías Martínez Moreno <159188838+matias-martinezm@users.noreply.github.com>
2024-03-21 20:49:47 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main{
   public static void Main(String[] args){
 
     System.out.println("El proyecto se encuentra en https://gallery.appinventor.mit.edu/?galleryid=6476639a-7baa-4cc1-a435-6ed9f062c588");
-    
+    System.out.println("se usa mit App inventor que consiste en la PROGRAMACION EN BLOQUES");
   }
 }
